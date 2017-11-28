8bc9acde18ce3e2341001d794493460be944e42d
TanguyH <tanguyh@icloud.com>
2017-11-28 14:56:54 +0100

    Updated block hash: transaction hash (ok), nonce hash (ok, over 1 min).

--- GeorgeCoin/src/Miner/Miner.java
+++ GeorgeCoin/src/Miner/Miner.java
@@ -4,6 +4,12 @@ import Client.Client;
 import Server.ServerCore;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Miner {
     private Client client;
     private ServerCore server;
@@ -46,7 +52,12 @@ public class Miner {
         String Tx2=jsonObj.get("Tx2").toString();
         String Tx3=jsonObj.get("Tx3").toString();
         
-        mineBlock(Tx0, Tx1, Tx2, Tx3);
+        try {
+			encryptTransactions(Tx0, Tx1, Tx2, Tx3);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         String block= new JSONObject()
                 .put("type","Block")
@@ -62,10 +73,78 @@ public class Miner {
         client.sendMessage("relay",msg);
     }
     
-    public String mineBlock(String trans1, String trans2, String trans3, String trans4){
+    public String encryptTransactions(String trans1, String trans2, String trans3, String trans4) throws NoSuchAlgorithmException {
     	// ---------------------------------------
         // COMPUTATION OF THE BLOCK HERE
         // ---------------------------------------
-    	return "mined";
+    	MessageDigest md = MessageDigest.getInstance("SHA-256");
+    	
+    	// hash transactions two by two
+    	byte[] hash1 = encryptTransactionPair(trans1, trans2, md);
+    	byte[] hash2 = encryptTransactionPair(trans3, trans4, md);
+    	System.out.println(hash1 + " " + hash2);
+    	
+    	String hex1 = String.format( "%064x", new BigInteger( 1, hash1) );
+    	String hex2 = String.format( "%064x", new BigInteger( 1, hash2) );
+        System.out.println(hex1 + "\n" +  hex2);
+        
+        // hash resulting hashs
+        byte[] total_transaction_hash = encryptTransactionPair(hex1, hex2, md);
+        System.out.println("\n" + total_transaction_hash);
+        String total_hex = String.format( "%064x", new BigInteger( 1, total_transaction_hash) );
+        System.out.println("\n" +  total_hex);
+        
+        // hash with nonce to obtain POW approved hash
+        int size = 100000000;
+        Random nonce_generator = new Random();
+        int nonce = nonce_generator.nextInt(size);
+        System.out.println("nonce : " + nonce);
+                
+        int current = nonce;
+        boolean found_match = false;
+        String match = "none";
+        
+        while(current <= size && !found_match){
+        	String curr_try = tryBlockValidation(total_hex, current, md);
+        	if(curr_try.startsWith("000000")){
+        		System.out.println("try : " + curr_try);
+        		found_match = true;
+        		match = curr_try;
+        	}
+        	current++;
+        }
+        current = 0;
+        while(current < nonce && !found_match){
+        	String curr_try = tryBlockValidation(total_hex, current, md);
+        	if(curr_try.startsWith("000000")){
+        		System.out.println("try : " + curr_try);
+        		found_match = true;
+        		match = curr_try;
+        	}
+        	current++;
+        }
+        
+        if(found_match){
+        	System.out.println("found match : " + match);
+        }
+                
+        return "mined";
+    }
+    
+    public byte[] encryptTransactionPair(String trans1, String trans2, MessageDigest md) {
+    	String hash = trans1 + trans2;
+    	
+    	md.update(hash.getBytes(StandardCharsets.UTF_8));
+    	byte[] digest = md.digest();
+    	
+    	return digest;
+    }
+    
+    public String tryBlockValidation(String block_hash, int nonce, MessageDigest md) {
+    	String hash = block_hash + nonce;
+    	md.update(hash.getBytes(StandardCharsets.UTF_8));
+    	byte[] digest = md.digest();
+    	
+    	return String.format("%064x", new BigInteger( 1, digest));
     }
 }
